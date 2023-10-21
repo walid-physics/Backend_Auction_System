5409ced5a64a4aef1adf16b7c6020888fda3db3d
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-10-21 12:14:39 -0500

    Add files via upload

---
+++ AuctionSystem/src/main/java/miu/cs545/auctionsystem/model/Customer.java
@@ -0,0 +1,24 @@
+package com.example.project.Model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    private String license;
+
+}
