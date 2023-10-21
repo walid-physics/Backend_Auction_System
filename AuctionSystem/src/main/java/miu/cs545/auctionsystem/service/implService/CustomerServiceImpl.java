71b443e0af50b65918a5e9558369ddab86580e61
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-10-21 12:18:30 -0500

    Add files via upload

---
+++ AuctionSystem/src/main/java/miu/cs545/auctionsystem/service/implService/CustomerServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.project.Service.Customer;
+
+import com.example.project.Model.Customer;
+import com.example.project.Model.Product;
+import com.example.project.Repository.CustomerRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.expression.ExpressionException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+    private final CustomerRepository customerRepository;
+
+
+    @Override
+    public Optional<Customer> findCustomerById(Long id) throws Exception {
+        return customerRepository.findById(id);
+    }
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public void deleteCustomerById(Long id) {
+    customerRepository.deleteById(id);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer customer) throws Exception {
+        Customer existingCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new ExpressionException("Customer not found with id " + id));
+        existingCustomer.setFirstName(customer.getFirstName());
+        existingCustomer.setLastName(customer.getLastName());
+        existingCustomer.setLicense(customer.getLicense());
+        existingCustomer.setEmail(customer.getEmail());
+
+        return customerRepository.save(existingCustomer);
+    }
+}
