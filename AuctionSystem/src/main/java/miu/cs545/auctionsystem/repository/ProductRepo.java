42caffe233b84ac6b6a26df2e2827f4f0eea76f6
mahaj1 <134968479+mahaj1@users.noreply.github.com>
2023-10-21 12:29:55 -0500

    Update ProductRepo.java

--- AuctionSystem/src/main/java/miu/cs545/auctionsystem/repository/ProductRepo.java
+++ AuctionSystem/src/main/java/miu/cs545/auctionsystem/repository/ProductRepo.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Integer> {
+List<Product> findAllByNameContains(String name);
 
 }
