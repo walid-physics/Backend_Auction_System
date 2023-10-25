83e9f60fa57bd1c1cb8ee2192ffe85bfe7bb92af
ibrahimabuhdaib <ibrahim.abuhdiab@sg.com.jo>
2023-10-25 01:06:52 -0500

    fixed delete in product controller

--- AuctionSystem/src/main/java/miu/cs545/auctionsystem/contoller/ProductController.java
+++ AuctionSystem/src/main/java/miu/cs545/auctionsystem/contoller/ProductController.java
@@ -41,7 +41,7 @@ public class ProductController {
     public Product updateProduct(@RequestBody Product product) throws Exception {
         return productService.updateProduct(product);
     }
-    @DeleteMapping("/delete//{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteProduct(@PathVariable Integer id) throws Exception {
         productService.deleteProductById(id);
     }
